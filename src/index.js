76a97b3aa2f85d892cc083a7eaa923f3696389ec
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-27 19:28:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import DataProvider from "./context/DataProvider";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
