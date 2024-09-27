2632ceabc9a25f1e812010d09558adf008448112
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-27 19:26:45 +0530

    Update ProductDetail.js

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,30 +1,25 @@
+import React, { useContext } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import DataContext from '../context/DataContext'
 
-import React from 'react';
+const ProductDetails = () => {
+    const { id } = useParams()
+    const { data } = useContext(DataContext)
+    const product = data.find(el => el.id == id)
 
-const products = [
-  { id: 1, name: 'Samsung Galaxy S8', price: 16303, color: 'Black' },
-  { id: 2, name: 'Samsung Galaxy S9', price: 20888, color: 'Black' },
-  { id: 3, name: 'Samsung Galaxy S8+', price: 18701, color: 'Black' },
-];
-
-function ProductDetail({ match }) {
-  const productId = parseInt(match.params.id);
-  const product = products.find(p => p.id === productId);
-
-  return (
-    <div>
-      <h1>Product Detail</h1>
-      {product ? (
+    return (
         <div>
-          <h2>{product.name}</h2>
-          <p>Price: {product.price}</p>
-          <p>Color: {product.color}</p>
+            <div style={{ border: "1px solid black" }} >
+                <h3>{product.name}</h3>
+                <p>{product.desc}</p>
+                <b>{product.price}</b>
+                <img height="100px" src={product.image} alt="" />
+            </div>
+            <Link to={'/'} >
+                <button>More products</button>
+            </Link>
         </div>
-      ) : (
-        <p>Product not found!</p>
-      )}
-    </div>
-  );
+    )
 }
 
-export default ProductDetail;
+export default ProductDetails
